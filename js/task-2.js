6aeb280042657935804bb0fc5f064012d10fea98
Sakrid1s <bilinets.vitalik@gmail.com>
2024-01-04 21:09:16 +0200

    Done task 1-2

--- js/task-2.js
+++ js/task-2.js
@@ -11,6 +11,26 @@
 // removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів
 // у приватній властивості items об'єкта.
 
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items.splice(itemToRemove, 1);
+  }
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
